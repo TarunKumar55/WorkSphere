7092cc56594c807d1035c574f0d9a8a1ac747fed
TarunKumar55 <tarunkumarkoppuravuri@gmail.com>
2026-06-13 17:34:25 +0530

    Fix backend setup

--- server/configs/prisma.js
+++ server/configs/prisma.js
@@ -1,16 +1,16 @@
 import "dotenv/config";
-import { PrismaClient } from "./generated/prisma";
+import { PrismaClient } from "@prisma/client";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { neonConfig } from "@neondatabase/serverless";
+import ws from "ws";
 
-import es from "ws";
 neonConfig.webSocketConstructor = ws;
 
-const connectionString = `${process.env.DATABASE_URL}`;
-const adapter = new PrismaNeon({
-  connectionString,
-});
+const connectionString = process.env.DATABASE_URL;
+
+const adapter = new PrismaNeon({ connectionString });
 const prisma = global.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV === "development") global.prisma = prisma;
+
 export default prisma;
